e308aafc736027fb39754c3994a0e2b50e997d63
Alfeihmr <107840579+farelfair@users.noreply.github.com>
2025-06-28 11:58:47 +0800

    compile script generated flashable
    whats new ?
    
    - Clang 14
    - If the image exists then it will assume that the kernel was successfully compiled and vice versa.
    - kernel codename Siina

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# script made by farelfair
+# EndeavourOS Distro
 
 # setup color
 red='\033[0;31m'
@@ -10,6 +12,8 @@ yellow='\033[0;33m'
 WORK_DIR=$(pwd)
 KERN_IMG="${WORK_DIR}/out/arch/arm64/boot/Image"
 KERN_IMG2="${WORK_DIR}/out/arch/arm64/boot/Image.gz"
+ANYKERNEL_DIR="${WORK_DIR}/AnyKernel3"
+ZIP_NAME="Siina-$(date +%Y%m%d-%H%M).zip"
 
 # clone clang
 if [ ! -d "toolchain/clang-14" ]; then
@@ -41,6 +45,7 @@ export ANDROID_MAJOR_VERSION=r
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a05m_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j16
 
+# check image
 if [ -e "$KERN_IMG" ] || [ -e "$KERN_IMG2" ]; then
         echo -e "\n"
         echo -e "$green << compile kernel success! >> \n$white"
@@ -51,5 +56,15 @@ else
         echo -e "\n"
 fi
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
-cp out/arch/arm64/boot/Image $(pwd)/AnyKernel3/
+cp "$KERN_IMG" "${WORK_DIR}/arch/arm64/boot/Image"
+cp "$KERN_IMG" "$ANYKERNEL_DIR/"
+
+cd "$ANYKERNEL_DIR" || exit
+zip -r9 "../$ZIP_NAME" ./* > /dev/null
+
+cd "$WORK_DIR" || exit
+
+rm -f "$ANYKERNEL_DIR/Image"
+rm -f arch/arm64/boot/Image
+
+echo -e "${green} kernel generated: $ZIP_NAME${white}"
